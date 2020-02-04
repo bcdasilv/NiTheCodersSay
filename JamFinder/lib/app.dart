b78453e617824f3696498676aa8a3c03ce5a3c1a
Stephen <smparkin@calpoly.edu>
2020-02-04 10:21:07 -0800

    revert back to material, add tabs

--- JamFinder/lib/app.dart
+++ JamFinder/lib/app.dart
@@ -1,60 +1,80 @@
-import 'package:flutter/cupertino.dart';
-import 'styles.dart';
-import 'ChatView.dart';
-import 'JamView.dart';
-import 'DiscoverView.dart';
+import 'package:flutter/material.dart';
+import 'chatView.dart';
+import 'jamView.dart';
+import 'discoverView.dart';
 
 class JamFinderApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        return CupertinoApp(
-            home: JamFinderHome(),
+        return MaterialApp(
+            title: 'JamFinder',
+            theme: ThemeData(
+                primarySwatch: Colors.indigo,
+            ),
+            home: JamFinderTabView(title: 'JamFinder'),
         );
     }
 }
 
-class JamFinderHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.conversation_bubble),
-            title: Text('Chat'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.music_note),
-            title: Text('Jam'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.location),
-            title: Text('Discover'),
-          ),
-        ],
-      ),
-      tabBuilder: (context, index) {
-        switch (index) {
-          case 0:
-            return CupertinoTabView(builder: (context) {
-              return CupertinoPageScaffold(
-                child: ChatView(),
-              );
-            });
-          case 1:
-            return CupertinoTabView(builder: (context) {
-              return CupertinoPageScaffold(
-                child: JamView(),
-              );
-            });
-          case 2:
-            return CupertinoTabView(builder: (context) {
-              return CupertinoPageScaffold(
-                child: DiscoverView(),
-              );
-            });
-        }
-      },
-    );
-  }
+class JamFinderTabView extends StatefulWidget {
+    JamFinderTabView({Key key, this.title}) : super(key: key);
+    final String title;
+    @override
+    _JamFinderTabViewState createState() => _JamFinderTabViewState();
+}
+
+class _JamFinderTabViewState extends State<JamFinderTabView> {
+    int _selectedIndex = 0;
+    static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+    static const List<Widget> _widgetOptions = <Widget>[
+        Text(
+            'Index 0: Chat',
+            style: optionStyle,
+        ),
+        Text(
+            'Index 1: Jam',
+            style: optionStyle,
+        ),
+        Text(
+            'Index 2: Discover',
+            style: optionStyle,
+        ),
+    ];
+
+    void _onItemTapped(int index) {
+        setState(() {
+            _selectedIndex = index;
+        });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('JamFinder'),
+            ),
+            body: Center(
+                child: _widgetOptions.elementAt(_selectedIndex),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.chat),
+                        title: Text('Chat'),
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.music_note),
+                        title: Text('Jam'),
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.map),
+                        title: Text('Discover'),
+                    ),
+                ],
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.indigo[800],
+                onTap: _onItemTapped,
+            ),
+        );
+    }
 }
