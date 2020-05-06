dc427e86dd51b6ab0332e48cae8ad9a61ab7ed05
Chloe Hebert <chhebert@calpoly.edu>
2020-05-06 19:06:40 -0400

    correct yaml

--- Jammies/test_driver/app.dart
+++ Jammies/test_driver/app.dart
@@ -1,88 +1,13 @@
 import 'package:Jammies/editProfileView.dart';
 import 'package:flutter/material.dart';
-import 'chatView.dart';
-import 'jamView.dart';
-import 'loginView.dart';
-import 'registerView.dart';
-import 'discoverView.dart';
-import 'initialView.dart';
-import 'myProfileView.dart';
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:Jammies/main.dart' as app;
 
-class JamFinderApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Jammies',
-      theme: ThemeData(
-//        brightness: Brightness.dark,
-        primarySwatch: Colors.indigo,
-      ),
-      initialRoute: '/',
-      routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => initialView(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        '/discover': (context) => discoverView(),
-        '/login': (context) => loginField(),
-        '/register': (context) => registerField(),
-        '/jam': (context) => jamView(),
-        '/chat': (context) => chatView(),
-        '/myProfileView': (context) => myProfileView(),
-        '/editProfileView': (context) => editProfileView(),
-      },
-    );
-  }
-}
+void main() {
+  // This line enables the extension.
+  enableFlutterDriverExtension();
 
-class JamFinderTabView extends StatefulWidget {
-  JamFinderTabView({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _JamFinderTabViewState createState() => _JamFinderTabViewState();
-}
-
-class _JamFinderTabViewState extends State<JamFinderTabView> {
-  int _selectedIndex = 0;
-  List<Widget> _widgetOptions = <Widget>[
-    chatView(),
-    jamView(),
-    discoverView(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Jammies'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            title: Text('Chat'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.music_note),
-            title: Text('Jam'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            title: Text('Discover'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.indigo[800],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
+  // Call the `main()` function of the app, or call `runApp` with
+  // any widget you are interested in testing.
+  app.main();
 }
