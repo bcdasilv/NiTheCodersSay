03b351ea0808c6b53ffd0b317d8522044fcd1320
Sarah Samora <sarahsamora@mail.com>
2020-05-24 13:56:38 -0700

    Fix sent match data (#143)
    * fixed format of matchting data
    
    * bad merge on smart

--- Jammies/lib/jamView.dart
+++ Jammies/lib/jamView.dart
@@ -237,8 +237,7 @@ class _JamView extends State<jamView>  {
 
     Map<String, String> header = {'email': email, 'password': password};
 
-
-    final response = await http.post('http://jam.smpark.in/match', headers: header, body: { 'matcher': globals.id, 'matchee': id.toString() } );
+    final response = await http.post('https://jam.smpark.in/match', headers: header, body: { 'matcher': globals.id, 'matchee': id.toString() } );
     if(response.statusCode == 800) {
       return   Alert(context: context,
         type: AlertType.error,
