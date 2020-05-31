3bc5eacf8ba23b2f9538cb367f61f5b0c6e9a7d1
Kush Upadhyay <38016332+k-upadhya@users.noreply.github.com>
2020-05-31 14:53:51 -0700

    Get and update user + Invalid zipcode check (#148)
    * fix backend stuff (#144)
    
    * Display profile photo universally and refresh profile pic on upload (#99) (#101)
    
    * Saving profile image to device
    
    
    
    * status badge
    
    * Travis (#92)
    
    * initial travis
    
    * fixed?
    
    * fixed 2 electric boogaloo
    
    * actually tho
    
    * more
    
    * yay
    
    * yay 2
    
    * yay 3
    
    * yay 4
    
    * Update README
    
    Add front and back-end build status
    
    * remove dupes
    
    * fix building
    
    * log out added to edit profile and fixed multiple paage issues
    
    * Fixed indentation
    
    * display default image if no image, otherwise image
    
    * globals test
    
    * display profile everywhere
    
    * download profile pic on login to display
    
    Co-authored-by: natei <nateirwin25@gmail.com>
    Co-authored-by: Chloe Hebert <43476327+hebertca18@users.noreply.github.com>
    Co-authored-by: Stephen Parkinson <smparkin@calpoly.edu>
    Co-authored-by: Nut <48338271+nateirwin25@users.noreply.github.com>
    
    Co-authored-by: natei <nateirwin25@gmail.com>
    Co-authored-by: Chloe Hebert <43476327+hebertca18@users.noreply.github.com>
    Co-authored-by: Stephen Parkinson <smparkin@calpoly.edu>
    Co-authored-by: Nut <48338271+nateirwin25@users.noreply.github.com>
    
    * quick build fix
    
    * Get matches (#97)
    
    * getMatches method
    
    * init v0
    
    * calls right method now
    
    * Added list of matched users
    
    * getMatches change
    
    Co-authored-by: Chloe Hebert <chhebert@calpoly.edu>
    Co-authored-by: k-upadhya <kupadhya@calpoly.edu>
    
    * Get matches (#102)
    
    * getMatches method
    
    * init v0
    
    * calls right method now
    
    * Added list of matched users
    
    * getMatches change
    
    Co-authored-by: Chloe Hebert <chhebert@calpoly.edu>
    Co-authored-by: k-upadhya <kupadhya@calpoly.edu>
    
    * cards with sample json
    
    * makePost v0 added
    
    * updated makePost v1
    
    * add getPost
    
    * fix quotes
    
    * register returns userid
    
    Co-authored-by: Sarah Samora <sarahsamora@mail.com>
    Co-authored-by: natei <nateirwin25@gmail.com>
    Co-authored-by: Chloe Hebert <43476327+hebertca18@users.noreply.github.com>
    Co-authored-by: Nut <48338271+nateirwin25@users.noreply.github.com>
    Co-authored-by: Chloe Hebert <chhebert@calpoly.edu>
    Co-authored-by: k-upadhya <kupadhya@calpoly.edu>
    Co-authored-by: k-upadhya <kpupadhyay17@gmail.com>
    
    * added get/updateUser
    
    * port change, being tested
    
    * added updateUser check for existing email
    
    * fixed updateUser check
    
    * Combine tasks (#147)
    
    * added zipcode check
    
    * tested
    
    Co-authored-by: k-upadhya <kupadhya@calpoly.edu>
    
    * fixed Response msgs and added zipcode check to updateUser
    
    * fixed dob output in getUser
    
    Co-authored-by: Stephen Parkinson <smparkin@calpoly.edu>
    Co-authored-by: Sarah Samora <sarahsamora@mail.com>
    Co-authored-by: natei <nateirwin25@gmail.com>
    Co-authored-by: Chloe Hebert <43476327+hebertca18@users.noreply.github.com>
    Co-authored-by: Nut <48338271+nateirwin25@users.noreply.github.com>
    Co-authored-by: Chloe Hebert <chhebert@calpoly.edu>
    Co-authored-by: k-upadhya <kupadhya@calpoly.edu>

--- flaskServer/app.py
+++ flaskServer/app.py
@@ -243,6 +243,9 @@ def register():
     except:
         return Response("{'error':'not all data provided'}", status=400, mimetype='application/json')
 
+    if not verifyZipcode(zipcode):
+        return Response("{'error':'Zipcode does not exist'}", status=423, mimetype='application/json')
+
     exists = db.session.query(db.exists().where(Users.email == email)).scalar()
     if exists:
         return Response("{'error':'User exists'}", status=422, mimetype='application/json')
@@ -256,7 +259,14 @@ def register():
     user = Users.query.filter_by(email=email).first()
     return Response("{'userid':'"+str(user.id)+"'}", status=200, mimetype='application/json')
 
+  
+def verifyZipcode(zip1):
+    z1 = select_zipcode(zip1)
+    if not (z1):
+        return False
+    return True
 
+  
 @app.route('/login', methods=["POST"])
 def login():
     if request.json != None:
@@ -358,13 +368,92 @@ def getProfile():
     bio = profile.bio
     return jsonify(name=name, about_me=about_me, bio=bio)
 
-def distance(zip1, zip2):
-        z1 = select_zipcode(zip1)
-        z2 = select_zipcode(zip2)
-        if not (z1) or not (z2):
-            return None
-        return haversine(z1['lat'], z1['long'], z2['lat'], z2['long'])
+  
+@app.route('/updateUser', methods=["POST"])
+def updateUser():
+    email = request.headers['email']
+    password = request.headers['password']
 
+    valid = verify(email, password)
+    if not valid:
+        return Response("{'error':'Incorrect email or password'}", status=401, mimetype='application/json')
+
+    if request.json != None:
+        data = request.json
+    else:
+        data = request.form
+
+    try:
+        newEmail = data['email']
+        username = data['username']
+        newPassword = data['password']
+        zipcode = data['zipcode']
+        dob = data['dob']
+        name = data['name']
+    except:
+        return Response("{'error':'Not all User fields provided'}", status=400, mimetype='application/json')
+
+    user = Users.query.filter_by(email=email).first()
+    if user == None:
+        return Response("{'error':'No such user'}", status=422, mimetype='application/json')
+
+    if email != newEmail:
+        exists = db.session.query(db.exists().where(Users.email == newEmail)).scalar()
+        if exists:
+            return Response("{'error':'Email already exists'}", status=409, mimetype='application/json')
+    
+    if not verifyZipcode(zipcode):         
+        return Response("{'error':'Zipcode does not exist'}", status=423, mimetype='application/json')
+
+    user.email = newEmail
+    user.username = username
+    user.password = newPassword
+    user.zipcode = zipcode
+    user.dob = dob
+    user.name = name
+
+    db.session.commit()
+    return Response("{'status':'User updated in db'}", status=200, mimetype='application/json')
+
+
+@app.route('/getUser', methods=["GET"])
+def getUser():
+    email = request.headers['email']
+    password = request.headers['password']
+    try:
+        userid = request.headers['userid']
+    except:
+        userid = None
+
+    valid = verify(email, password)
+    if not valid:
+        return Response("{'error':'Incorrect email or password'}", status=401, mimetype='application/json')
+
+    if userid == None:
+        user = Users.query.filter_by(email=email).first()
+    else:
+        user = Users.query.filter_by(id=userid).first()
+
+    if user == None:
+        return Response("{'error':'No such user'}", status=422, mimetype='application/json')
+
+    email = user.email
+    username = user.username
+    password = user.password
+    zipcode = user.zipcode
+    dob = user.dob
+    name = user.name
+    return jsonify(name=name, username=username, email=email, password=password, dob=str(dob), zipcode=zipcode)
+
+
+def distance(zip1, zip2):
+    z1 = select_zipcode(zip1)
+    z2 = select_zipcode(zip2)
+    if not (z1) or not (z2):
+        return None
+    return haversine(z1['lat'], z1['long'], z2['lat'], z2['long'])
+
+  
 @app.route('/getNearby', methods=["GET"])
 def getNearby():
     email = request.headers['email']
