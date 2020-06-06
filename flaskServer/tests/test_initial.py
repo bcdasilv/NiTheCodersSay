97e92e3144c93a93a8e45cefaa6b8c16fb7c2543
Stephen <smparkin@calpoly.edu>
2020-06-05 17:17:51 -0700

    Travis

--- flaskServer/tests/test_initial.py
+++ flaskServer/tests/test_initial.py
@@ -57,7 +57,7 @@ def test_match(client):
     header = {"email": "test", "password": "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"}
     body = {"matcher": 1, "matchee": 2}
     response = client.post("/match", headers=header, data=body)
-    assert response.status_code == 418
+    assert response.status_code == 200
 
 def test_getMatches(client):
     header = {"email": "test", "password": "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"}
