1c849107ebf5c043245f622602a15d67d5e0a367
Stephen <smparkin@calpoly.edu>
2020-06-05 17:13:42 -0700

    Travis

--- flaskServer/tests/test_initial.py
+++ flaskServer/tests/test_initial.py
@@ -21,6 +21,11 @@ def test_register(client):
     response = client.post("/register", data=body)
     assert response.status_code == 200
 
+def test_register(client):
+    body = {"email": "test2", "username": "test", "password": "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff", "zipcode": "93401", "dob": "2020-04-05", "name": "test"}
+    response = client.post("/register", data=body)
+    assert response.status_code == 200
+
 def test_login(client):
     body = {"email": "test", "password": "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"}
     response = client.post("/login", data=body)
